d9a034651f2baf601a9f4c6d2739dc68b90f460d
mfehr <mathieu.fehr@gmail.com>
2020-09-13 12:15:43 -0400

    Add base operations for obc dialect

--- lib/Obc/ObcOps.cpp
+++ lib/Obc/ObcOps.cpp
@@ -18,6 +18,51 @@
 using namespace mlir;
 using namespace obc;
 
+static void print(OpAsmPrinter &p, ObcMachine op) {
+  // Print the machine operation name
+  p << op.getOperationName() << ' ';
+
+  // Print the machine name
+  p.printSymbolName(
+      op.getAttrOfType<StringAttr>(::mlir::SymbolTable::getSymbolAttrName())
+          .getValue());
+
+  // Print the machine body
+  p.printRegion(op.body(),
+                /*printEntryBlockArgs=*/false,
+                /*printBlockTerminators=*/true);
+}
+
+static ParseResult parseObcMachine(OpAsmParser &parser,
+                                   OperationState &result) {
+  // Parse the machine name
+  StringAttr nameAttr;
+  if (parser.parseSymbolName(nameAttr, ::mlir::SymbolTable::getSymbolAttrName(),
+                             result.attributes)) {
+    return failure();
+  };
+
+  // Parse the machine body.
+  auto *body = result.addRegion();
+  return parser.parseRegion(*body, /*regionArgs*/ {}, /*argTypes*/ {});
+}
+
+static void print(OpAsmPrinter &p, ObcBody op) {
+  // Print the body operation name
+  p << op.getOperationName() << ' ';
+
+  // Print the body
+  p.printRegion(op.body(),
+                /*printEntryBlockArgs=*/false,
+                /*printBlockTerminators=*/true);
+}
+
+static ParseResult parseObcBody(OpAsmParser &parser, OperationState &result) {
+  // Parse the region.
+  auto *body = result.addRegion();
+  return parser.parseRegion(*body, /*regionArgs*/ {}, /*argTypes*/ {});
+}
+
 namespace mlir {
 namespace obc {
 #define GET_OP_CLASSES
